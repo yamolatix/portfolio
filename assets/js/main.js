442792c2f319637d6db4f042b31f2045a148b97a
Yamila Ortiz Toscano <yamilaortiztoscano@gmail.com>
2023-06-28 16:16:34 -0300

    img

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,21 +16,6 @@ jQuery(document).ready(function($) {
 	    }
 	}).scroll();
 
-/*	$('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(function() {
-			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-				if (target.length) {
-					$('html,body').animate({
-						scrollTop: target.offset().top - mastheadheight
-					}, 300);
-					return false;
-				}
-			}
-		});*/
-
-
-
 	// Cache selectors
 	var lastId,
 	    topMenu = $("#navbarResponsive"),
